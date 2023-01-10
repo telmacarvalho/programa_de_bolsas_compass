cb83f2a38c5fb1cb37ae39e40991791f01d3cbbb
Telma Carvalho <telmaclarita@gmail.com>
2023-01-10 13:28:20 -0300

    Adicionando exemplo da query 2 para o dashboard da seção 11

--- SQL/Ex_section11.sql
+++ SQL/Ex_section11.sql
@@ -35,6 +35,18 @@ left join payments
 -- (Query 2) Estados que mais venderam
 -- Colunas: país, estado, vendas (#)
 
+select
+	'Brazil' as país,
+	cus.state as estado,
+	count(fun.paid_date) as "vendas (#)"
+
+from sales.funnel as fun
+left join sales.customers as cus
+	on fun.customer_id = cus.customer_id
+where paid_date between '2021-08-01' and '2021-08-31'
+group by país, estado
+order by "vendas (#)" desc
+limit 5
 
 -- (Query 3) Marcas que mais venderam no mês
 -- Colunas: marca, vendas (#)
