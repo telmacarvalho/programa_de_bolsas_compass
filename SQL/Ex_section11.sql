b1d7ad3ecaf753426f5f9d1bf251f43915c336c1
Telma Carvalho <telmaclarita@gmail.com>
2023-01-10 16:33:35 -0300

    Adicionando projeto de dashboard da seção 11

--- SQL/Ex_section11.sql
+++ SQL/Ex_section11.sql
@@ -51,10 +51,47 @@ limit 5
 -- (Query 3) Marcas que mais venderam no mês
 -- Colunas: marca, vendas (#)
 
+select
+	pro.brand as marca,
+	count(fun.paid_date) as "vendas (#)"
+from sales.funnel as fun
+left join sales.products as pro
+	on fun.product_id = pro.product_id
+where paid_date between '2021-08-01' and '2021-08-31'
+group by marca
+order by "vendas (#)" desc
+limit 5
 
 -- (Query 4) Lojas que mais venderam
 -- Colunas: loja, vendas (#)
 
+select
+	sto.store_name as loja,
+	count(fun.paid_date) as "vendas (#)"
+from sales.funnel as fun
+left join sales.stores as sto
+	on fun.store_id = sto.store_id
+where paid_date between '2021-08-01' and '2021-08-31'
+group by loja
+order by "vendas (#)" desc
+limit 5
 
 -- (Query 5) Dias da semana com maior número de visitas ao site
 -- Colunas: dia_semana, dia da semana, visitas (#)
+
+select
+	extract('dow' from visit_page_date) as dia_semana,
+	case 
+		when extract('dow' from visit_page_date)=0 then 'domingo'
+		when extract('dow' from visit_page_date)=1 then 'segunda'
+		when extract('dow' from visit_page_date)=2 then 'terça'
+		when extract('dow' from visit_page_date)=3 then 'quarta'
+		when extract('dow' from visit_page_date)=4 then 'quinta'
+		when extract('dow' from visit_page_date)=5 then 'sexta'
+		when extract('dow' from visit_page_date)=6 then 'sábado'
+		else null end as "dia da semana",
+	count(*) as "visitas (#)"
+from sales.funnel
+where visit_page_date between '2021-08-01' and '2021-08-31'
+group by dia_semana
+order by dia_semana
