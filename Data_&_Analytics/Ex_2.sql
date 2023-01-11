23d93d4bbec5bffc0d124c880cbc9528feca0aa0
Telma Carvalho <telmaclarita@gmail.com>
2023-01-11 11:26:03 -0300

    Adicionando exercício 2

---
+++ Data_&_Analytics/Ex_2.sql
@@ -0,0 +1,5 @@
+--Ex_2
+--
+--Apresente a query para listar os 10 livros mais caros. 
+--Ordenar as linhas pela coluna valor, em ordem decrescente.  
+--Atenção às colunas esperadas no resultado final:  titulo, valor.
