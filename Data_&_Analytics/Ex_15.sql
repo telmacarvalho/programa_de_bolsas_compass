5a6f4a182bb7201cc04eca5927df39ff2b2437f7
Telma Carvalho <telmaclarita@gmail.com>
2023-01-13 17:59:23 -0300

    Adicionando exercício 15

---
+++ Data_&_Analytics/Ex_15.sql
@@ -0,0 +1,9 @@
+-- Ex_15
+--
+--Apresente a query para listar os códigos das vendas identificadas como deletadas. 
+--Apresente o resultado em ordem crescente.
+
+select
+	cdven
+from tbvendas 
+where deletado = 1
