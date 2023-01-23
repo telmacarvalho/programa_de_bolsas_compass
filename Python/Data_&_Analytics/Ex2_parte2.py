62ffc9275103f0030f85ee14748d77b71a08b914
Telma Carvalho <telmaclarita@gmail.com>
2023-01-23 12:23:29 -0300

    Adicionando exercício 2 parte 1 da seção 3

---
+++ Python/Data_&_Analytics/Ex2_parte2.py
@@ -0,0 +1,19 @@
+# Exercícios Parte 1
+
+# Escreva um código Python que verifica se os números 0, 7851
+# e 9 elevado na potência 3  são pares ou ímpares.
+# Para cada número, imprima o Par: ou Ímpar: e o número correspondente.
+
+# Exemplo de formato de saída:
+
+# Par: 2
+
+# Ímpar: 3
+
+numeros = [0, 7851, 9 ** 3]
+
+for i in numeros:
+    if (i % 2) == 0:
+        print(f'Par: {i}')
+    else:
+        print(f'Ímpar: {i}')
