64f313a16d14ec10a0bb80779e40e7779140bea6
Telma Carvalho <telmaclarita@gmail.com>
2023-01-23 12:31:24 -0300

    Adicionando exercício 3 parte 1 da seção 3

--- Python/Data_&_Analytics/Ex3_parte1.py
+++ Python/Data_&_Analytics/Ex3_parte1.py
@@ -1,5 +1,10 @@
 # Exercícios Parte 1
-# Escreva um código Python que imprime os números pares de 0 até 20 (incluso).
 
+# Escreva um código Python que imprime os números pares de 0 até 20
+# (incluso).
 
 # Dica: olhe a documentação da função range().
+
+for i in range(21):
+    if (i % 2) == 0:
+        print(i)
