583b2ba272e53756e0060550f67ddbb001505984
Telma Carvalho <telmaclarita@gmail.com>
2023-01-24 08:54:30 -0300

    Adicionando exercício 5 da parte 1 da seção 3

---
+++ Python/Data_&_Analytics/Ex5_parte1.py
@@ -0,0 +1,11 @@
+# Exercícios 5 Parte 1
+
+# Escreva um código Python que tem 3 variáveis dia (22),
+# mês(10) e ano(2022) e imprime a data completa no formato a seguir:
+# Exemplo: 22/10/2022
+
+dia = 22
+mes = 10
+ano = 2022
+
+print(f'{dia}/{mes}/{ano}')
