f83ff7c9f4a4962c4a2f9fbd42a8743caf89a346
Telma Carvalho <telmaclarita@gmail.com>
2023-01-28 13:32:55 -0300

    Alterando o nome dos arquivos de Ex.._parte1 para Ex.._python1

---
+++ Python/Data_&_Analytics/Ex19_python1.py
@@ -0,0 +1,23 @@
+# Exercício 19 Parte 2
+
+# Calcule o valor mínimo, valor máximo, valor médio e a mediana da lista
+# gerada na célula abaixo:
+
+# Obs.: Lembrem-se, para calcular a mediana a lista deve estar ordenada!
+
+# import random
+# # amostra aleatoriamente 50 números do intervalo 0...500
+# random_list = random.sample(range(500),50)
+
+# Use as variáveis abaixo para representar cada operação matemática
+
+# mediana
+# media
+# valor_minimo
+# valor_maximo
+
+import random
+# amostra aleatoriamente 50 números do intervalo 0...500
+random_list = random.sample(range(500), 50)
+
+
