b28ee70540dda6a5b2c7066da281c8537b93c1c5
Telma Carvalho <telmaclarita@gmail.com>
2023-01-30 09:25:54 -0300

    Adicionando exercício 20 da seção 4

---
+++ Python/Data_&_Analytics/Ex20.py
@@ -0,0 +1,10 @@
+# Exercício 20 Parte 2
+
+# Imprima a lista abaixo de trás para frente.
+
+# a = [1, 0, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
+a = [1, 0, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
+list_invert = a[::-1]
+print(list_invert)
