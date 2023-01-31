1767411e1104ddd09e6e365ae10dac32b970032a
Telma Carvalho <telmaclarita@gmail.com>
2023-01-30 22:02:16 -0300

    Adicionado exercícios 1 ao 3 da tarefa 1 da seção 7 (em desenvolvimento)

--- Python/Data_&_Analytics/Desafio/Tarefa1.py
+++ Python/Data_&_Analytics/Desafio/Tarefa1.py
@@ -14,12 +14,12 @@ with open('actors.csv', 'r') as arquivo:
 # -----------------------------------------------------------------------------
 # 1- O ator/atriz com maior número de filmes e o respectivo número de filmes.
 
-# Ordena a matriz em ordem descrescente pela coluna 'Number of Movies'
+# Ordena a matriz pela ordem descrescente da coluna 'Number of Movies'
 filmes = sorted(atores, key=lambda filmes: filmes[2], reverse=True)
 
 # Armazena em uma variável um texto informando o nome e quantidade de filmes
 # da primeira linha, convertendo os valores para string e as concatenando
-dado = str(filmes[0][0]) + ' é o(a) ator/atriz com maior número de filmes' +\
+dado = filmes[0][0] + ' é o(a) ator/atriz com maior número de filmes' +\
     ' com um total de ' + str(filmes[0][2]) + ' filmes.'
 
 # Armazena o resultado acima em um arquivo txt
@@ -43,13 +43,38 @@ media_filmes = round(sum(total_filmes)/len(atores), 2)
 # Armazena em uma variável um texto informando o nome e quantidade de filmes
 # da primeira linha, convertendo os valores para string e as concatenando
 dado = 'A média do número de filmes por autor é ' + str(media_filmes) + '.'
-print(dado)
 
 # Armazena o resultado acima em um arquivo txt
 with open('tarefa1_2.txt', 'w') as arquivo:
     arquivo.write(dado)
 
 # -----------------------------------------------------------------------------
-# O ator/atriz com a maior média por filme.
-# print(atores[0][1])
-# print(type(atores[0][1]))
+# 3- O ator/atriz com a maior média por filme.
+
+# Ordena a matriz pela ordem descrescente da coluna 'Average per Movie'
+media = sorted(atores, key=lambda filmes: filmes[3], reverse=True)
+
+# Armazena em uma variável um texto informando o nome do ator da
+# primeira linha do resultado anterior
+dado = media[0][0] + ' é o (a) ator/atriz com a maior média por filme.'
+
+# Armazena o resultado acima em um arquivo txt
+with open('tarefa1_3.txt', 'w') as arquivo:
+    arquivo.write(dado)
+
+# -----------------------------------------------------------------------------
+# 4- O nome do(s) filme(s) mais frequente(s) e sua respectiva frequência.
+
+frequencia_filme = []
+for linha in atores:
+    if linha[4] not in frequencia_filme:
+        frequencia_filme.append([linha[4], 1])
+    else:
+        print('TESTE')
+        for i in frequencia_filme:
+            if i[0] == i[4]:
+                contador = i[1]+1
+                frequencia_filme.append((i[0], contador))
+                print(i[0])
+
+print(frequencia_filme)
