503292c97bcbf483672be6103d8ddf87bb532c4d
Telma Carvalho <telmaclarita@gmail.com>
2023-04-19 12:23:29 -0300

    Refatorando código e modelagem lógica da tarefa 1

--- Sprint_9/Tarefa_1.sql
+++ Sprint_9/Tarefa_1.sql
@@ -1,7 +1,79 @@
--- Exibe todos os dados da tabela tb_locacao ordenado pelo seu id a fim de verificar se é preciso colocar na primeira forma (FN1)
+-- Exibe todos os dados da tabela tb_locacao ordenado pelo seu id a fim de verificar se é preciso colocar 
+-- na primeira forma (FN1)
 SELECT * 
 FROM tb_locacao tl
 ORDER BY idLocacao 
+
+-- Exibe os atributos do objeto idLocacao com dependências únicas ordenado pelo seu id, sem a coluna qdtDiaria, 
+-- que pode ser calculado a partir diferença entre as datas de locação e de entrega, e formatando as colunas 
+-- dataLocacao e dataEntrega para a forma ano-mês-dia com a fim de separar suas dependências na segunda forma (FN2)
+SELECT 
+	idLocacao,
+	idCliente,
+	idCarro,
+	kmCarro,
+	substr(dataLocacao, 1, 4) || '-' || substr(dataLocacao, 5, 2) || '-' || substr(dataLocacao, 7, 2) AS dataLocacao,
+	horaLocacao,
+	vlrDiaria, 
+	substr(dataEntrega, 1, 4) || '-' || substr(dataLocacao, 5, 2) || '-' || substr(dataLocacao, 7, 2) AS dataEntrega,
+	horaEntrega,
+	idVendedor 
+FROM tb_locacao
+ORDER BY idLocacao
+
+-- Cria uma nova tabela de locacao com dependências únicas aplicando a segunda forma (FN2) 
+CREATE TABLE tb_locacao_v2 (
+  idLocacao INTEGER PRIMARY KEY,
+  idCliente INTEGER,
+  idCarro INTEGER,
+  idVendedor INTEGER,
+  kmCarro INTEGER,
+  vlrDiaria DECIMAL(18,2),
+  dataLocacao TEXT,
+  horaLocacao TIME,
+  dataEntrega TEXT,
+  horaEntrega TIME,
+  FOREIGN KEY (idCliente) REFERENCES tb_cliente(idCliente),
+  FOREIGN KEY (idCarro) REFERENCES tb_carro(idCarro),
+  FOREIGN KEY (idVendedor) REFERENCES tb_vendedor(idVendedor)
+)
+
+-- Exibe os tipos de dados das colunas da tabela criada anteriormente
+PRAGMA TABLE_INFO(tb_locacao_v2)
+
+-- Adiciona dados da tabela tb_locacao na tabela tb_Locacao_v2
+INSERT INTO tb_Locacao_v2 
+	(
+	idLocacao, 
+	idCliente, 
+	idCarro, 
+	kmCarro, 
+	dataLocacao ,
+	horaLocacao,
+	vlrDiaria,
+	dataEntrega,
+	horaEntrega,
+	idVendedor 
+	)
+SELECT 
+	idLocacao, 
+	idCliente, 
+	idCarro, 
+	kmCarro, 
+	substr(dataLocacao, 1, 4) || '-' || substr(dataLocacao, 5, 2) || '-' || substr(dataLocacao, 7, 2),
+	horaLocacao,
+	vlrDiaria,
+	substr(dataEntrega, 1, 4) || '-' || substr(dataLocacao, 5, 2) || '-' || substr(dataLocacao, 7, 2),
+	horaEntrega,
+	idVendedor 
+FROM tb_locacao
+ORDER BY idLocacao
+
+
+-- Exibe a tabela tb_locacao_v2 
+SELECT *
+FROM tb_locacao_v2
+
 
 -- Exibe os atributos do objeto idCliente ordenado pelo seu id a fim de separar suas dependências na segunda forma (FN2)
 SELECT 
@@ -14,8 +86,26 @@ FROM tb_locacao
 ORDER BY idCliente 
 
 -- Cria nova tabela de clientes sem duplicidades aplicando a segunda forma (FN2)
-CREATE TABLE tb_cliente
-AS SELECT 
+CREATE TABLE tb_cliente (
+	idCliente INTEGER PRIMARY KEY,
+	nomeCliente TEXT,
+	cidadeCliente TEXT,
+	estadoCliente TEXT,
+	paisCliente TEXT
+)
+
+-- Exibe os tipos de dados das colunas da tabela criada anteriormente
+PRAGMA TABLE_INFO(tb_cliente)
+
+-- Adiciona dados da tabela tb_locacao na tabela tb_cliente
+INSERT INTO tb_cliente(
+	idCliente,
+	nomeCliente,
+	cidadeCliente,
+	estadoCliente,
+	paisCliente	
+)
+SELECT 
 		idCliente,
 		nomeCliente,
 		cidadeCliente,
@@ -44,8 +134,29 @@ ORDER BY idCarro
 
 -- Cria nova tabela de carros sem duplicidades aplicando a segunda forma (FN2) e sem o atibuto tipoCombustível, 
 -- o qual será atribuído em outra tabela, eliminando, assim, as dependências transitivas na terceira forma (FN3)
-CREATE TABLE tb_carro 
-AS SELECT 
+CREATE TABLE tb_carro (
+  idCarro INTEGER PRIMARY KEY,
+  classiCarro TEXT,
+  marcaCarro TEXT,
+  modeloCarro TEXT,
+  anoCarro INTEGER,
+  idcombustivel INTEGER,
+  FOREIGN KEY (idcombustivel) REFERENCES tb_combustivel(idcombustivel)
+)
+
+-- Exibe os tipos de dados das colunas da tabela criada anteriormente
+PRAGMA TABLE_INFO(tb_carro)
+
+-- Adiciona dados da tabela tb_locacao na tabela tb_carro
+INSERT INTO tb_carro (
+	idCarro,
+	classiCarro,
+	marcaCarro,
+	modeloCarro,
+	anoCarro,
+	idcombustivel
+)
+SELECT 
 		idCarro,
 		classiCarro,
 		marcaCarro,
@@ -69,10 +180,22 @@ FROM tb_locacao
 ORDER BY idcombustivel 
 
 -- Cria nova tabela de combustível sem duplicidades aplicando a terceira forma (FN3)
-CREATE TABLE tb_combustivel
-AS SELECT 
-		idcombustivel,
-		tipoCombustivel 
+CREATE TABLE tb_combustivel(
+	idcombustivel INTEGER PRIMARY KEY,
+	tipoCombustivel INTEGER
+)
+
+-- Exibe os tipos de dados das colunas da tabela criada anteriormente
+PRAGMA TABLE_INFO(tb_combustivel)
+
+-- Adiciona dados da tabela tb_locacao na tabela tb_combustivel
+INSERT INTO tb_combustivel(
+	idcombustivel,
+	tipoCombustivel
+)
+SELECT 
+	idcombustivel,
+	tipoCombustivel 
 FROM tb_locacao
 GROUP BY idcombustivel 
 ORDER BY idcombustivel 
@@ -92,8 +215,24 @@ FROM tb_locacao
 ORDER BY idVendedor 
 
 -- Cria nova tabela de vendedores sem duplicidades aplicando a segunda forma (FN2)
-CREATE TABLE tb_vendedor
-AS SELECT 
+CREATE TABLE tb_vendedor (
+	idVendedor INTEGER PRIMARY KEY,
+	nomeVendedor,
+	sexoVendedor,
+	estadoVendedor
+)
+
+-- Exibe os tipos de dados das colunas da tabela criada anteriormente
+PRAGMA TABLE_INFO(tb_vendedor)
+
+-- Adiciona dados da tabela tb_locacao na tabela tb_vendedor
+INSERT INTO tb_vendedor (
+	idVendedor,
+	nomeVendedor,
+	sexoVendedor,
+	estadoVendedor
+)
+SELECT 
 		idVendedor,
 		nomeVendedor,
 		sexoVendedor,
@@ -104,47 +243,3 @@ GROUP BY idVendedor
 -- Exibe a tabela tb_vendedores
 SELECT *
 FROM tb_vendedor
-
-
--- Exibe todos os dados da tabela tb_locacao 
-SELECT * 
-FROM tb_locacao tl
-ORDER BY idLocacao 
-
--- Exibe os atributos do objeto idLocacao com dependências únicas ordenado pelo seu id, sem a coluna qdtDiaria, 
--- que pode ser calculado a partir das datas de locação e ede entrega, e formatando as colunas dataLocacao e dataEntrega 
--- para a forma ano-mês-dia
-SELECT 
-	idLocacao,
-	idCliente,
-	idCarro,
-	kmCarro,
-	substr(dataLocacao, 1, 4) || '-' || substr(dataLocacao, 5, 2) || '-' || substr(dataLocacao, 7, 2) AS dataLocacao,
-	horaLocacao,
-	vlrDiaria, 
-	substr(dataEntrega, 1, 4) || '-' || substr(dataLocacao, 5, 2) || '-' || substr(dataLocacao, 7, 2) AS dataEntrega,
-	horaEntrega,
-	idVendedor 
-FROM tb_locacao
-ORDER BY idLocacao
-
--- Cria uma nova tabela de locacao com dependências únicas aplicando a segunda forma (FN2)
-CREATE TABLE tb_locacao_v2
-AS SELECT 
-		idLocacao,
-		idCliente,
-		idCarro,
-		kmCarro,
-		substr(dataLocacao, 1, 4) || '-' || substr(dataLocacao, 5, 2) || '-' || substr(dataLocacao, 7, 2) AS dataLocacao,
-		horaLocacao,
-		qtdDiaria,
-		vlrDiaria,
-		substr(dataEntrega, 1, 4) || '-' || substr(dataLocacao, 5, 2) || '-' || substr(dataLocacao, 7, 2) AS dataEntrega,
-		horaEntrega,
-		idVendedor 
-FROM tb_locacao
-ORDER BY idLocacao 
-
--- Exibe a tabela tb_locacao_v2 
-SELECT *
-FROM tb_locacao_v2
--- Sprint_9/Tarefa_2.sql
+++ Sprint_9/Tarefa_2.sql
@@ -2,3 +2,55 @@
 SELECT * 
 FROM tb_locacao tl
 ORDER BY idLocacao 
+
+
+-- Exibe dados da dimensão data de locação
+SELECT 
+	substr(dataLocacao, 1, 4) || '-' || substr(dataLocacao, 5, 2) || '-' || substr(dataLocacao, 7, 2) AS dataLocacao,
+	substr(dataLocacao, 7, 2) AS diaLocacao,
+--	CASE
+--		WHEN (SELECT substr(dataLocacao, 7, 2) < '7') THEN 1
+--		WHEN (SELECT substr(dataLocacao, 7, 2) BETWEEN '7' AND '14') THEN 2
+--	END AS semanaLocacao,
+	substr(dataLocacao, 5, 2) AS mesLocacao,
+	substr(dataLocacao, 1, 4) AS anoLocacao
+FROM tb_locacao 
+--GROUP BY dataLocacao
+ORDER BY dataLocacao
+
+-- tb_locacao_v2 definition
+
+
+
+-- Cria nova dimensão de clientes sem duplicidades
+CREATE TABLE dim_cliente
+AS SELECT 
+		idCliente,
+		nomeCliente,
+		cidadeCliente,
+		estadoCliente,
+		paisCliente
+FROM tb_locacao 
+GROUP BY idCliente 
+ORDER BY idCliente 
+
+-- Exibe a tabela tb_clientes
+SELECT *
+FROM dim_cliente
+
+
+-- Cria nova dimensão de datas sem duplicidades
+CREATE TABLE dim_cliente
+AS SELECT 
+		idCliente,
+		nomeCliente,
+		cidadeCliente,
+		estadoCliente,
+		paisCliente
+FROM tb_locacao 
+GROUP BY idCliente 
+ORDER BY idCliente 
+
+-- Exibe a tabela tb_clientes
+SELECT *
+FROM dim_cliente
