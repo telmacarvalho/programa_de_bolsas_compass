aecbfd3f6d556b0bae773676cf293da81eef8ded
Telma Carvalho <telmaclarita@gmail.com>
2023-04-20 12:34:27 -0300

    Adicionando tarefa 2 (em desenvolvimento)

--- Sprint_9/Tarefa_2.sql
+++ Sprint_9/Tarefa_2.sql
@@ -3,26 +3,64 @@ SELECT *
 FROM tb_locacao tl
 ORDER BY idLocacao 
 
+-- Exibe dimensões e mátricas do fato da locacao ordenado pelo seu id
+SELECT 
+	idLocacao,
+	idCliente,
+	idCarro,
+	dataLocacao,
+	horaLocacao,
+	dataEntrega,
+	horaEntrega,
+	idVendedor
+	kmCarro,
+	vlrDiaria
+FROM tb_locacao
+ORDER BY idLocacao
+
+-- Cria a tabela do fato de locacao 
+CREATE TABLE fato_locacao (
+  idLocacao INTEGER PRIMARY KEY,
+  idCliente INTEGER,
+  idCarro INTEGER,
+  idVendedor INTEGER,
+  dataLocacao TEXT,
+  horaLocacao TIME,
+  dataEntrega TEXT,
+  horaEntrega TIME,
+  kmCarro INTEGER,
+  vlrDiaria DECIMAL(18,2),
+  FOREIGN KEY (idCliente) REFERENCES dim_cliente(idCliente),
+  FOREIGN KEY (idCarro) REFERENCES dim_carro(idCarro),
+  FOREIGN KEY (idVendedor) REFERENCES dim_vendedor(idVendedor),
+  FOREIGN KEY (dataLocacao) REFERENCES dim_dataLocacao(dataLocacao),
+  FOREIGN KEY (dataEntrega) REFERENCES dim_dataEntrega(dataEntrega),
+  FOREIGN KEY (dataEntrega) REFERENCES dim_dataEntrega(dataEntrega),
+  FOREIGN KEY (horaLocacao) REFERENCES dim_horaEntrega(horaLocacao),
+  FOREIGN KEY (horaEntrega) REFERENCES dim_horaEntrega(horaEntrega),
+)
+
+-- Exibe os tipos de dados das colunas da tabela criada anteriormente
+PRAGMA TABLE_INFO(tb_locacao_v2)
+
 
 -- Exibe dados da dimensão data de locação
 SELECT 
 	substr(dataLocacao, 1, 4) || '-' || substr(dataLocacao, 5, 2) || '-' || substr(dataLocacao, 7, 2) AS dataLocacao,
 	substr(dataLocacao, 7, 2) AS diaLocacao,
---	CASE
---		WHEN (SELECT substr(dataLocacao, 7, 2) < '7') THEN 1
---		WHEN (SELECT substr(dataLocacao, 7, 2) BETWEEN '7' AND '14') THEN 2
---	END AS semanaLocacao,
 	substr(dataLocacao, 5, 2) AS mesLocacao,
 	substr(dataLocacao, 1, 4) AS anoLocacao
-FROM tb_locacao 
---GROUP BY dataLocacao
+FROM tb_locacao
 ORDER BY dataLocacao
 
--- tb_locacao_v2 definition
+CREATE VIEW nome_da_view AS dim_datas
+SELECT coluna1, coluna2, ...
+FROM tabela
+WHERE condição;
 
 
 
--- Cria nova dimensão de clientes sem duplicidades
+-- Cria nova dimensão de clientes 
 CREATE TABLE dim_cliente
 AS SELECT 
 		idCliente,
@@ -39,18 +77,4 @@ SELECT *
 FROM dim_cliente
 
 
--- Cria nova dimensão de datas sem duplicidades
-CREATE TABLE dim_cliente
-AS SELECT 
-		idCliente,
-		nomeCliente,
-		cidadeCliente,
-		estadoCliente,
-		paisCliente
-FROM tb_locacao 
-GROUP BY idCliente 
-ORDER BY idCliente 
 
--- Exibe a tabela tb_clientes
-SELECT *
-FROM dim_cliente
